7781e52f96e8d51eab139a6dc10261df0e5678be
Marek Ciupak <6394528+marekciupak@users.noreply.github.com>
2021-09-11 00:13:57 +0200

    Initialize mix package

---
+++ lib/blocker_lists.ex
@@ -0,0 +1,18 @@
+defmodule BlockerLists do
+  @moduledoc """
+  Documentation for `BlockerLists`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BlockerLists.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/blocker_lists_test.exs
@@ -0,0 +1,8 @@
+defmodule BlockerListsTest do
+  use ExUnit.Case
+  doctest BlockerLists
+
+  test "greets the world" do
+    assert BlockerLists.hello() == :world
+  end
+end
