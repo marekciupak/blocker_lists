aa0f57194b3dfb0affe1b0f2ada0509c64beac5a
Marek Ciupak <6394528+marekciupak@users.noreply.github.com>
2025-07-27 12:18:51 +0200

    Migrate to v2 cert domains

--- lib/blocker_lists/sources/cert_polska.ex
+++ lib/blocker_lists/sources/cert_polska.ex
@@ -1,6 +1,6 @@
 defmodule BlockerLists.Sources.CertPolska do
   def domains do
-    %{body: body, status_code: 200} = HTTPoison.get!("https://hole.cert.pl/domains/domains.txt")
+    %{body: body, status_code: 200} = HTTPoison.get!("https://hole.cert.pl/domains/v2/domains.txt")
 
     body
     |> String.split("\n", trim: true)
