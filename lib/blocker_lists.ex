2caf5dc7e7d25c8f1d05fb718422dce06990d405
Marek Ciupak <6394528+marekciupak@users.noreply.github.com>
2025-07-27 12:55:46 +0200

    Add sanitization

--- lib/blocker_lists.ex
+++ lib/blocker_lists.ex
@@ -2,12 +2,16 @@ defmodule BlockerLists do
   alias BlockerLists.Sources
 
   def regenerate do
-    domains = Sources.StevenBlack.domains() ++ Sources.CertPolska.domains()
-    domains_with_social = Sources.StevenBlack.domains_with_social() ++ Sources.CertPolska.domains()
+    domains = (Sources.StevenBlack.domains() ++ Sources.CertPolska.domains()) |> sanitize()
+    domains_with_social = (Sources.StevenBlack.domains_with_social() ++ Sources.CertPolska.domains()) |> sanitize()
 
     File.write!("blockerList.json", build_ios(domains))
     File.write!("brave.txt", build_brave(domains))
     File.write!("brave-with-social.txt", build_brave(domains_with_social))
+  end
+
+  defp sanitize(domains) do
+    Enum.filter(domains, &String.match?(&1, ~r/^[[:alnum:]\_\-\.]+$/))
   end
 
   defp build_ios(domains) do
