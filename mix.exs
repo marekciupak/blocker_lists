1d18b3c4d51650d6f98e8580e0aece3dd35bacb7
Marek Ciupak <6394528+marekciupak@users.noreply.github.com>
2025-07-27 12:26:27 +0200

    Upgrade runtimes and deps

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule BlockerLists.MixProject do
     [
       app: :blocker_lists,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
